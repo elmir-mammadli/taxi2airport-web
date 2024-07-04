da5b8a51073ce3e6341f184024be227b7faebcc1
Elmir Mammadli <101706322+elmir-mammadli@users.noreply.github.com>
2024-07-04 22:27:25 +0200

    change: hero img format to webp

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -30,7 +30,7 @@
       </div>
       <div>
         <span class="relative">
-          <NuxtImg src="/images/hero_vector.png" format="webp" class="hidden md:block" width="540" alt="hero image" />
+          <NuxtImg src="/images/hero_vector.webp" class="hidden md:block" width="540" alt="hero image" />
         </span>
       </div>
     </div>
